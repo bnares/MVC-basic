0c6a85f300e2fc689d4c5699c2918303b114a367
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-05-21 12:44:10 +0200

    new

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+//echo "Requested URL = ".$_SERVER['QUERY_STRING'];
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+//echo get_class($router);
+
+//Add the routes 
+
+$router ->add('',['controller'=>'Home', 'action'=>'index']);
+$router ->add('posts', ['controller'=>'Posts', 'action'=>'index']);
+$router ->add('posts/new', ['controller'=>'Posts', 'action'=>'new']);
+
+//display the routing table
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+//Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url))
+{
+	echo '<pre>';
+	var_dump($router->getParams());
+	echo '</pre>';
+}
+else
+{
+	echo 'No route found for URL: '.$url;
+}
+
+?>
